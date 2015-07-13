152cc165a332510d2389e6ffd53e4843b44b78ac
Simon Beal <sb48@sanger.ac.uk>
2015-07-13 11:52:27 +0100

    Update gh-pages Mirrors github.com/muddyfish/muddyfish.github.io Add new logo and different versions of it Update css to be 'more minimal' Get readme file dynamically from github master branch There is a failover in case the cors proxy site goes down Add showdown markdown to html converter

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+var current_project = location.pathname.split("/")[1];
+var corsproxy = "http://crossorigin.me/";
+var github_url = "https://github.com/cancerit/".concat(current_project, "/raw/master/README.md");
+var final_url = corsproxy.concat(github_url);
+var error_html = "<small>Using fail-over page generation, please notify <a href='mailto:cgp-it@sanger.ac.uk' target='_top'>cgp-it@sanger.ac.uk</a></small><br>";
+$.get(final_url)
+    .success(function (response) {
+	    var converter = new showdown.Converter(),
+		html = converter.makeHtml(response);
+	    $("#content").html(html);
+	})
+    .error(function () {
+	    var xhr = new XMLHttpRequest();
+	    xhr.open('GET', 'https://api.github.com/repos/cancerit/'.concat(current_project, '/readme?ref=master'));
+	    xhr.setRequestHeader("Accept", "application/vnd.github.3.html");
+	    xhr.setRequestHeader("User-Agent", "CancerIT")
+		xhr.send();
+	    
+	    xhr.onload = function(e){
+		$('#content').replaceWith(error_html.concat(xhr.response));
+	    }
+	});
