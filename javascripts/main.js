35dd2309e312434067ef7ac883e1962b6fadc2d0
Simon Beal <sb48@sanger.ac.uk>
2015-07-13 13:04:54 +0100

    Update order in which the readme is attempted to be pulled First test the official github api Then try pulling the raw using a proxy service

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,14 @@
-var current_project = location.pathname.split("/")[1];
+//var current_project = location.pathname.split("/")[1];
+var current_project = "cgpPindel"
+
+var github_api_url = 'https://api.github.com/repos/cancerit/'.concat(current_project, '/readme?ref=master');
+
 var corsproxy = "http://crossorigin.me/";
 var github_url = "https://github.com/cancerit/".concat(current_project, "/raw/master/README.md");
-var final_url = corsproxy.concat(github_url);
+var proxy_url = corsproxy.concat(github_url);
+
 var error_html = "<small>Using fail-over page generation, please notify <a href='mailto:cgp-it@sanger.ac.uk' target='_top'>cgp-it@sanger.ac.uk</a></small><br>";
+/*
 $.get(final_url)
     .success(function (response) {
 	    var converter = new showdown.Converter(),
@@ -19,4 +25,23 @@ $.get(final_url)
 	    xhr.onload = function(e){
 		$('#content').replaceWith(error_html.concat(xhr.response));
 	    }
+	});
+*/
+$.ajax({
+	url:github_api_url,
+	headers: {
+	    "Accept": "application/vnd.github.3.html",
+	    "User-Agent": "CancerIT"
+	}
+    })
+    .success(function (response) {
+	    $('#content').replaceWith(response); 
+	})
+    .error(function (r) {
+	    $.get(proxy_url)
+		.success(function (response) {
+			var converter = new showdown.Converter(),
+			    html = converter.makeHtml(response);
+			$("#content").html(html);
+		    });
 	});
